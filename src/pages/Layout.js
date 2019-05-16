bd7c750afff2387ea9d9ddc10db3f29059b43362
john fredy <jfguzman@cognox.com>
2019-05-16 11:59:42 -0500

    cambio en el layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,58 +1,32 @@
-import React, { useState } from 'react';
-import { Layout as LayoutDocument, Menu, Icon } from 'antd';
+import React from 'react';
+import { Layout as LayoutDocument, Menu, Breadcrumb } from 'antd';
 
-const { Header, Sider, Content } = LayoutDocument;
+const { Header, Footer, Content } = LayoutDocument;
 
-const Document = () =>{
-  
-  //const [state, setState] = useState(false);
- let state = false;
-  const toggle = () => {
-    //setState(!state.collapsed);
-    state = true;
-  };
+const Document = () => {
 
-    return(
-    <di>
-     <LayoutDocument>
-        <Sider trigger={null} collapsible collapsed={state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <LayoutDocument>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <Icon
-              className="trigger"
-              type={state.collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={toggle}
-            />
-          </Header>
-          <Content
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              background: '#fff',
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
-        </LayoutDocument>
-      </LayoutDocument>
-    </di>);
+  return (
+    <LayoutDocument>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">Proyección financiera</Menu.Item>
+          <Menu.Item key="2">Acerda de</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+        </Breadcrumb>
+        <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Evaluación de proyectos ©2019-1 Creado por:
+      <p>John <br /> Ana <br/>Alex <br/>Esteban <br/>Ivan</p></Footer>
+    </LayoutDocument>);
 }
 
 export default Document;
